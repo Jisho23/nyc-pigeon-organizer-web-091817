62a6e54ff559f5f67c8445f805c45b49a8936fab
Joshua Denenberg <j.denenberg42@gmail.com>
2017-09-13 14:40:20 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
         organized_list[name] = {} unless organized_list[name]
         organized_list[name][key] = [] unless organized_list[name][key]
         organized_list[name][key] << value.to_s
-        binding.pry
+
       end
     end
   end

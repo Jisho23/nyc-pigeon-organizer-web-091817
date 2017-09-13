c9a6400f59e765810cfa4c567f8be6946e826d20
Joshua Denenberg <j.denenberg42@gmail.com>
2017-09-13 14:40:33 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   organized_list = {}
   data.each do |key, hash|
-    h.each do |value, names|
+    hash.each do |value, names|
       names.each do |name|
         organized_list[name] = {} unless organized_list[name]
         organized_list[name][key] = [] unless organized_list[name][key]
